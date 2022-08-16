0a84d46fc0474a9ae48331419c4e3ad896830bb5
koviatsinets <koviatsinets@gmail.com>
2022-08-16 15:05:53 +0300

    feat: add SPA

---
+++ rslang/src/components/app/routing.ts
@@ -0,0 +1,66 @@
+export function switchToStateFromURLHash() {
+    (document.querySelector('#login') as HTMLElement).addEventListener('click', switchToLogin);
+    (document.querySelector('#book') as HTMLElement).addEventListener('click', switchToBook);
+    (document.querySelector('#audiocall') as HTMLElement).addEventListener('click', switchToAudiocall);
+    (document.querySelector('#sprint') as HTMLElement).addEventListener('click', switchToSprint);
+    (document.querySelector('#statistic') as HTMLElement).addEventListener('click', switchToStatistic);
+    (document.querySelector('#main') as HTMLElement).addEventListener('click', switchToMain);
+
+    let SPAState = { pagename: '' };
+    const contentBlock = document.querySelector('.content') as HTMLElement;
+    const URLHash = window.location.hash;
+    const stateStr = URLHash.substring(1);
+    if (stateStr != '') {
+        const parts = stateStr.split('_');
+        SPAState = { pagename: parts[0] };
+    } else SPAState = { pagename: 'main' };
+    switch (SPAState.pagename) {
+        case 'main':
+            contentBlock.innerHTML = `<div class="main">Main</div>`;
+            break;
+        case 'login':
+            contentBlock.innerHTML = `<div class="login">Login</div>`;
+            break;
+        case 'book':
+            contentBlock.innerHTML = `<div class="book">Book</div>`;
+            break;
+        case 'audiocall':
+            contentBlock.innerHTML = `<div class="audiocall">Audiocall</div>`;
+            break;
+        case 'sprint':
+            contentBlock.innerHTML = `<div class="sprint">Sprint</div>`;
+            break;
+        case 'statistic':
+            contentBlock.innerHTML = `<div class="statistic">Statistic</div>`;
+            break;
+    }
+}
+
+function switchToState(newState: { pagename: string }) {
+    const stateStr = newState.pagename;
+    location.hash = stateStr;
+}
+
+export function switchToMain() {
+    switchToState({ pagename: 'main' });
+}
+
+export function switchToLogin() {
+    switchToState({ pagename: 'login' });
+}
+
+export function switchToBook() {
+    switchToState({ pagename: 'book' });
+}
+
+export function switchToAudiocall() {
+    switchToState({ pagename: 'audiocall' });
+}
+
+export function switchToSprint() {
+    switchToState({ pagename: 'sprint' });
+}
+
+export function switchToStatistic() {
+    switchToState({ pagename: 'statistic' });
+}
--- rslang/src/index.ts
+++ rslang/src/index.ts
@@ -1,3 +1,8 @@
 import './style.scss';
+import { switchToStateFromURLHash } from './components/app/routing';
+
+window.location.hash = 'main';
+window.onhashchange = switchToStateFromURLHash;
+switchToStateFromURLHash();
 
 console.log('Hello');
