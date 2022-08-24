2346dc5cc1610e5f5ba30e04726b9882d3fb123a
koviatsinets <koviatsinets@gmail.com>
2022-08-25 02:25:45 +0300

    refactor: move svg in constants.ts

--- rslang/src/components/audiocall-page/audiocall-page.ts
+++ rslang/src/components/audiocall-page/audiocall-page.ts
@@ -1,7 +1,7 @@
 import { renderPageContent } from '../../utils/ui';
 import { getRandomNum } from '../../utils/common';
 import { getAllAggregatedWords } from '../../api/users-aggregated-words';
-import { path, contentDifficult } from '../../utils/constants';
+import { path, contentDifficult, svgImage } from '../../utils/constants';
 import { Word } from '../../models/types';
 import './audiocall-page.scss';
 
@@ -40,15 +40,7 @@ export const renderAudiocallPage = (): void => {
         const contentGame = `
         <div class="game-content">
             <div class="audio-card">
-                <div class="word-audio" data-word="${currentWord['wordTranslate']}">
-                    <?xml version="1.0" encoding="UTF-8" standalone="no"?>
-                    <svg xmlns="http://www.w3.org/2000/svg" version="1.0"  width="60" height="60" viewBox="0 0 75 75">
-                    <path d="M39.389,13.769 L22.235,28.606 L6,28.606 L6,47.699 L21.989,47.699 L39.389,62.75 L39.389,13.769z"
-                    style="stroke:#111;stroke-width:5;stroke-linejoin:round;fill:#111;"
-                    />
-                    <path d="M48,27.6a19.5,19.5 0 0 1 0,21.4M55.1,20.5a30,30 0 0 1 0,35.6M61.6,14a38.8,38.8 0 0 1 0,48.6" style="fill:none;stroke:#111;stroke-width:5;stroke-linecap:round"/>
-                    </svg>
-                </div>
+                <div class="word-audio" data-word="${currentWord['wordTranslate']}">${svgImage}</div>
                 <div class="block-words">
                     <div class="word-answer">${arrWords[0].wordTranslate}</div>
                     <div class="word-answer">${arrWords[1].wordTranslate}</div>
--- rslang/src/utils/constants.ts
+++ rslang/src/utils/constants.ts
@@ -1,4 +1,4 @@
-export { path, contentDifficult };
+export { path, contentDifficult, svgImage };
 
 const path = 'https://rslangforrsschool.herokuapp.com';
 
@@ -16,4 +16,14 @@ const contentDifficult = `
         </div>
     </div>
 </div>
+`;
+
+const svgImage = `
+    <?xml version="1.0" encoding="UTF-8" standalone="no"?>
+    <svg xmlns="http://www.w3.org/2000/svg" version="1.0"  width="60" height="60" viewBox="0 0 75 75">
+    <path d="M39.389,13.769 L22.235,28.606 L6,28.606 L6,47.699 L21.989,47.699 L39.389,62.75 L39.389,13.769z"
+    style="stroke:#111;stroke-width:5;stroke-linejoin:round;fill:#111;"
+    />
+    <path d="M48,27.6a19.5,19.5 0 0 1 0,21.4M55.1,20.5a30,30 0 0 1 0,35.6M61.6,14a38.8,38.8 0 0 1 0,48.6" style="fill:none;stroke:#111;stroke-width:5;stroke-linecap:round"/>
+    </svg>
 `;
