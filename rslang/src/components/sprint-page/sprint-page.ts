121db055b62ac743d243b56a3613dc3dd01ba4f4
EvgeniyaDanilovich <evgeniyadanilovich@gmail.com>
2022-08-30 17:01:02 +0300

    feat: add functional

--- rslang/src/components/sprint-page/sprint-page.ts
+++ rslang/src/components/sprint-page/sprint-page.ts
@@ -5,12 +5,10 @@ import { contentDifficult } from '../../utils/constants';
 import './sprint-page.scss';
 
 let group = 0;
-// const currentChunkWords: Word[] = [];
 
-export function initSprintPage() {
-    // renderPageContent(contentDifficult);
-    // document.querySelectorAll('.button-level').forEach((el) => el.addEventListener('click', selectLevel));
-    renderSprintPage();
+export function initSprintPage(): void {
+    renderPageContent(contentDifficult);
+    document.querySelectorAll('.button-level').forEach((el) => el.addEventListener('click', selectLevel));
 }
 
 function selectLevel(EO: Event): void {
@@ -20,7 +18,7 @@ function selectLevel(EO: Event): void {
 
 const somePages: number[] = [];
 
-async function getRandomPages() {
+function getRandomPages(): void {
     for (let i = 0; i < 5; i++) {
         let randomPage = getRandomNum(0, 29);
 
@@ -29,18 +27,8 @@ async function getRandomPages() {
         }
         somePages.push(randomPage);
     }
-    // for (const page of somePages) {
-    //     const dataWords: Word[] = await getChunkWords(group, page);
-    //     for (const word of dataWords) {
-    //         currentChunkWords.push(word);
-    //     }
-    // }
-    // return currentChunkWords;
 }
-// getWords().then((arr: Word[]) => {
-//     const cardNum = 0;
-//     renderSprintCard(arr[cardNum]);
-// });
+
 getRandomPages();
 
 const dataWords1: Word[] = await getChunkWords(group, somePages[0]);
@@ -51,55 +39,120 @@ const dataWords5: Word[] = await getChunkWords(group, somePages[4]);
 
 const allWords: Word[] = dataWords1.concat(dataWords2, dataWords3, dataWords4, dataWords5);
 
-const score = 0;
-const scoreAdd = 10;
+let score = 0;
+let scoreAdd = 10;
 
-const renderWords = (englishWord: string, russianWord: string) => `
+let isCorrectTranslate = true;
+let currentTranslate = isCorrectTranslate;
+setInterval((): void => {
+    isCorrectTranslate ? (isCorrectTranslate = false) : (isCorrectTranslate = true);
+}, 3000);
+
+const cardWords = (englishWord: string, russianWord: string): string => `
     <div class="english-word">${englishWord}</div>
     <div class="russian-word">${russianWord}</div>
 `;
 
-const sprintCard = (englishWord: string, russianWord: string) => `
+function renderCardWords(word: Word, wordFalse: Word): string {
+    currentTranslate = isCorrectTranslate;
+    if (isCorrectTranslate) {
+        return cardWords(word.word, word.wordTranslate);
+    } else {
+        return cardWords(word.word, wordFalse.wordTranslate);
+    }
+}
+
+const sprintCard = (): string => `
         <div class="sprint-card">
     <div class="score">${score}</div>
     <div class="add-score">+ <span>${scoreAdd}</span> points</div>
     <div class="stage">
-        <div class="stage__point active"></div>
-        <div class="stage__point"></div>
-        <div class="stage__point"></div>
+        <div class="stage__point point-1" data-point="1"></div>
+        <div class="stage__point point-2" data-point="2"></div>
+        <div class="stage__point point-3" data-point="3"></div>
     </div>
-       ${renderWords(englishWord, russianWord)}
+    <div class="words-wrapper">
+       ${renderCardWords(allWords[getRandomNum(0, 99)], allWords[getRandomNum(0, 99)])}
+    </div>
     <div class="answer">
         <div class="answer__right btn ">Right</div>
         <div class="answer__wrong btn">Wrong</div>
     </div>
     </div> `;
 
-let isCorrectTranslate = true;
-setInterval(() => {
-    isCorrectTranslate ? (isCorrectTranslate = false) : (isCorrectTranslate = true);
-}, 3000);
-
-function renderSprintCard(word: Word, wordFalse: Word) {
-    if (isCorrectTranslate) {
-        return sprintCard(word.word, word.wordTranslate);
-    } else {
-        return sprintCard(word.word, wordFalse.wordTranslate);
-    }
-}
-
-const randomWordsNum = getRandomNum(0, 99);
-
-function renderSprintPage() {
+function renderSprintPage(): void {
     const content = `
             <div class="sprint-wrapper">
                <div class="timer"></div>
                <div class="card-wrapper">
-                  ${renderSprintCard(allWords[randomWordsNum], allWords[randomWordsNum])}
+                  ${sprintCard()}
                </div>
             </div>`;
 
     renderPageContent(content);
+    listenEvents();
 }
 
-const rightBtn = document.querySelector('.answer__right');
+function listenEvents(): void {
+    function changeWord(): void {
+        const w = renderCardWords(allWords[getRandomNum(0, 99)], allWords[getRandomNum(0, 99)]);
+        const wordsWrapper = document.querySelector('.words-wrapper') as HTMLElement;
+        wordsWrapper.innerHTML = w;
+    }
+
+    function changeScore(): void {
+        score += scoreAdd;
+        const scoreText = document.querySelector('.score') as HTMLElement;
+        scoreText.textContent = `${score}`;
+    }
+
+    let quantityRightAnswer = 0;
+
+    function resetStage(): void {
+        const pointsList = document.querySelectorAll('.stage__point') as NodeListOf<Element>;
+
+        pointsList.forEach((point) => {
+            point.classList.remove('active');
+        });
+        quantityRightAnswer = 0;
+    }
+
+    function updateStage(): void {
+        quantityRightAnswer++;
+        const point = document.querySelector(`.point-${quantityRightAnswer}`) as HTMLElement;
+        point.classList.add('active');
+
+        if (quantityRightAnswer % 3 === 0) {
+            setTimeout(() => {
+                scoreAdd += 10;
+                const scoreAddText = document.querySelector('.add-score span') as HTMLElement;
+                scoreAddText.textContent = `${scoreAdd}`;
+                resetStage();
+            }, 500);
+        }
+    }
+
+    function resetScoreAdd(): void {
+        scoreAdd = 10;
+        const scoreAddText = document.querySelector('.add-score span') as HTMLElement;
+        scoreAddText.textContent = `${scoreAdd}`;
+    }
+
+    function oneStepGame(result: boolean) {
+        if (currentTranslate === result) {
+            changeScore();
+            updateStage();
+        } else {
+            resetStage();
+            resetScoreAdd();
+        }
+
+        changeWord();
+    }
+
+    const rightBtn = document.querySelector('.answer__right') as HTMLElement;
+    rightBtn.addEventListener('click', (): void => oneStepGame(true));
+
+    const wrongBtn = document.querySelector('.answer__wrong') as HTMLElement;
+    wrongBtn.addEventListener('click', (): void => oneStepGame(false));
+}
